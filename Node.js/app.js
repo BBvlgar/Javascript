5fa60038daf4995dd969441cf8b7d4f3885afb27
Denislav Petkov <dddenislav@gmail.com>
2019-10-31 20:33:36 +0200

    Create app.js

---
+++ Node.js/app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const port = 3000;
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    fs.readFile('index.html', (error, data) => {
+
+        if(error){
+            res.writeHead(404)
+            res.write('Error: File Not Found' + error)
+        } else{
+            res.write(data)
+        }
+        res.end();
+    })
+});
+
+server.listen(port , (error) => {
+
+    if(error){
+        console.log('Something went wrong', error)
+    } else{
+        console.log('Server is listening on port ' + port)
+    }
+});
+
