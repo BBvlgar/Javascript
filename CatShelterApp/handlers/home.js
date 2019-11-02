b42307bbf693a5c449704909f59c3fb7e336342b
Denislav Petkov <dddenislav@gmail.com>
2019-11-02 17:06:40 +0200

    Update home.js

--- CatShelterApp/handlers/home.js
+++ CatShelterApp/handlers/home.js
@@ -12,6 +12,7 @@ if(pathname === '/' && req.method === 'GET'){
     let filePath = path.normalize(
         path.join(__dirname, '../views/home/index.html')
     )
+    let modifiedData = '';
 
     fs.readFile(filePath, (err, data) => {
 
@@ -21,16 +22,29 @@ if(pathname === '/' && req.method === 'GET'){
             res.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
-
             res.write('404 Not found');
             res.end();
+            
             return;
         }
+        console.log(cats)
+        let modifiedCats = cats.map((cat) => `<li>
+    <img src=https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg alt="${cat.name}">
+    <h3>${cat.name}</h3>
+    <p><span>Breed: </span>${cat.breed}</p>
+    <p><span>Description: </span>${cat.description}</p>
+    <ul class="buttons">
+        <li class="btn edit"><a href="/cats-edit/${cat.name}">Change Info</a></li>
+        <li class="btn delete><a href="/cats-find-new-home/${cat.name}">New Home</a></li>
+        </ul>
+        </li>`);
+        let modifiedData = data.toString().replace('{{cats}}', modifiedCats);
 
         res.writeHead(200, { 'Content-Type': 'text/html'});
 
-        res.write(data);
+        res.write(modifiedData);
         res.end()
+
     })
 } else {
     return true;
