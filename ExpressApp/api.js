6efea4735dcffefc02e08bc80d026266eb872f3d
Denislav Petkov <dddenislav@gmail.com>
2019-11-03 03:23:10 +0200

    Update api.js

--- ExpressApp/api.js
+++ ExpressApp/api.js
@@ -10,4 +10,10 @@ router.get('/user', (req, res) => {
     res.send(users)
 })
 
+router.get('/user/:id', (req, res) => {
+
+    const user = users.find(u => u.id === +req.params.id)
+    res.send(users)
+})
+
 module.exports = router;
