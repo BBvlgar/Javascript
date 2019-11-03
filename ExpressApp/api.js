5a33877ec1d91d10ac16bf9143a06185407c337c
Denislav Petkov <dddenislav@gmail.com>
2019-11-03 21:15:59 +0200

    Update api.js

--- ExpressApp/api.js
+++ ExpressApp/api.js
@@ -1,19 +1,24 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
 
-const users = [
-    {name:"pesho", age: 3},
-    {name:"Ghosho", age: 4},
-]
-    
+const users = [{name:'Pesho', age:3},{name:'Ivan', age:4}]
+
 router.get('/user', (req, res) => {
-    res.send(users)
+    res.sendFile(__dirname + '/public/index.html')
 })
 
-router.get('/user/:id', (req, res) => {
+router.post('/user', (req, res) => {
 
-    const user = users.find(u => u.id === +req.params.id)
-    res.send(users)
+    console.log(req.body)
+
+})
+
+router.get('/user/:age', (req, res) => {
+
+    let names = users.find( u => u.age === +req.params.age)
+        console.log(names)
+
+        res.send(names.name + " " + names.age)
 })
 
 module.exports = router;
