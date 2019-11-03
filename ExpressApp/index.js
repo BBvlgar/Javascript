7061609420a36678c551efc39a784b7247043ad3
Denislav Petkov <dddenislav@gmail.com>
2019-11-03 04:11:31 +0200

    Update index.js

--- ExpressApp/index.js
+++ ExpressApp/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const router = require('./api.js');
+const api = require('./api.js');
 
 const port = 3000;
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
 
-app.use('/u', router)
-app.get('/', (req, res) => {
+function defaultHandler (req, res){
+    res.sendFile(__dirname + '/views/index.html');
+}
 
-    res.send('Hello world!');
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
+})
 
+app.use('/api', api);
+
+app.get('/',defaultHandler);
+
+app.use( (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(err.message);
 })
 
 app.listen(port , () => {
